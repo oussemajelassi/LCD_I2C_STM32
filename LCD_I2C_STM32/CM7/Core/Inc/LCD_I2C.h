f120ef5b0b7d6cd7e4f78947cab3141fafce5940
oussemajelassi <oussjls57@gmail.com>
2023-10-04 11:21:04 +0100

    Added Constructor

--- LCD_I2C_STM32/CM7/Core/Inc/LCD_I2C.h
+++ LCD_I2C_STM32/CM7/Core/Inc/LCD_I2C.h
@@ -13,9 +13,15 @@
 #define LCD_I2C_SEND_CMD_EN_LOW   0x08
 #define LCD_I2C_SEND_DATA_EN_HIGH 0x0D
 #define LCD_I2C_SEND_DATA_EN_LOW  0x09
+#define LCD_I2C_CLEAR_CMD 	      0x01
+#define LCD_I2C_RETURN_HOME_CMD	  0x02
 
 
-#define SLAVE_ADDRESS_LCD 0x4E
+#define SLAVE_ADDRESS_LCD 		  0x4E
+
+#include <cstring>
+
+
 typedef enum
 {
 	LCD_I2C_OKAY,
@@ -33,15 +39,17 @@ public :
 	char * LCD_I2C_THIRD_LINE  ;
 	char * LCD_I2C_FOURTH_LINE ;
 public :
+	LCD_I2C(void) ;
 	LCD_I2C_STATE LCD_I2C_SEND_STRING (char * str) ;
 	LCD_I2C_STATE LCD_I2C_SEND_CMD  (char Command ) ;
 	LCD_I2C_STATE LCD_I2C_SEND_DATA (char Command ) ;
 	LCD_I2C_STATE LCD_I2C_INIT (void ) ;
-	void LCD_I2C_SET_FIRST_LINE (void) ;
-	void LCD_I2C_SET_SECOND_LINE (void) ;
-	void LCD_I2C_SET_THIRD_LINE (void) ;
-	void LCD_I2C_SET_FOURTH_LINE (void) ;
-
+	void LCD_I2C_SET_FIRST_LINE  (const char *) ;
+	void LCD_I2C_SET_SECOND_LINE (const char *) ;
+	void LCD_I2C_SET_THIRD_LINE  (const char *) ;
+	void LCD_I2C_SET_FOURTH_LINE (const char *) ;
+	void LCD_I2C_CLEAR ( void ) ;
+	void LCD_I2C_UPDATE_SCREEN ( void ) ;
 
 };
 
